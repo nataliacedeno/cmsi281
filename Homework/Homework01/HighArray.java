829595f9401ae470dbe749f6d013ce6cd0157ede
hecrabbs <hecrabbs@gmail.com>
2020-09-19 17:44:08 -0700

    homework01 working

---
+++ Homework/Homework01/HighArray.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+
+public class HighArray {
+    private long[] a;
+    private int nElems;
+
+    public HighArray(int max) {
+        a = new long[max];
+        nElems = 0;
+    }
+
+    public boolean find(long searchKey) {
+        int j;
+        for (j = 0; j < nElems; j++)
+            if (a[j] == searchKey)
+                break;
+        if (j == nElems)
+            return false;
+        else
+            return true;
+    }
+
+    public void insert(long value) {
+        a[nElems] = value;
+        nElems++;
+    }
+
+    public boolean delete(long value) {
+        int j;
+        for (j = 0; j < nElems; j++)
+            if (value == a[j])
+                break;
+        if (j == nElems)
+            return false;
+        else {
+            for (int k = j; k < nElems; k++)
+                a[k] = a[k + 1];
+            nElems--;
+            return true;
+        }
+    }
+
+    public void display() {
+        for (int j = 0; j < nElems; j++) {
+            System.out.print(a[j] + " ");
+        }
+        System.out.println("");
+    }
+
+    public long getMax() {
+        if (nElems == 0) {
+            return -1;
+        } else {
+            return Arrays.stream(a).max().getAsLong();
+        }
+    }
+
+    public void noDups() {
+        for (int i = 0; i < nElems; i++) {
+            for (int j = i + 1; j < nElems; j++) {
+                if (a[i] == a[j]) {
+                    delete(a[j]);
+                    j--;
+                    i--;
+                }
+            }
+        }
+    }
+
+}
