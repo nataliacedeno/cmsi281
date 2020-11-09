dd4a70ea399630c0953c7473b1bb4f021de228a6
hecrabbs <hecrabbs@gmail.com>
2020-11-09 13:48:41 -0800

    Added classwork09

---
+++ Classwork/Classwork09/BinaryTree.java
@@ -0,0 +1,27 @@
+public class BinaryTree {
+
+    public static void postOrderPrinter(BinaryTreeNode node) {
+        if (node == null) {
+            return;
+        }
+        postOrderPrinter(node.getChild("L"));
+        postOrderPrinter(node.getChild("R"));
+        System.out.print("[" + node.getData() + "]");
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNode root = new BinaryTreeNode(0);
+        root.add(1, "L");
+        root.add(2, "R");
+        BinaryTreeNode one = root.getChild("L");
+        BinaryTreeNode two = root.getChild("R");
+        one.add(3, "L");
+        one.add(4, "R");
+        two.add(5, "L");
+        two.add(6, "R");
+        BinaryTreeNode six = two.getChild("R");
+        six.add(7, "L");
+        six.add(8, "R");
+        postOrderPrinter(root);
+    }
+}
