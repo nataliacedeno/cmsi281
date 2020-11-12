37c6fafe0c53b47871c8f0b7619523542b8a1d07
hecrabbs <hecrabbs@gmail.com>
2020-11-12 12:57:34 -0800

    Huffman almost works

--- Homework/Homework03/HuffmanApp.java
+++ Homework/Homework03/HuffmanApp.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class HuffmanApp {
     public static Map<Character, Integer> sortedMap = new LinkedHashMap<>();
-    public static Map<Character, Integer> encodedMap = new LinkedHashMap<>();
+    public static Map<Character, String> encodedMap = new LinkedHashMap<>();
     public static ArrayList<Tree> priorityQueue = new ArrayList<>();
     public static Tree huffmanTree;
     public static String message;
@@ -66,59 +66,81 @@ public class HuffmanApp {
             priorityQueue.add(newTree);
             Collections.sort(priorityQueue, Comparator.comparingInt((e) -> e.root.count));
 
-            huffmanTree = priorityQueue.get(0);
         }
+        huffmanTree = priorityQueue.get(0);
     }
 
     public static void encodeTree() {
         Tree t = huffmanTree;
-        Map m = sortedMap;
+        encodedMap = new LinkedHashMap<>();
+        for (Character c : sortedMap.keySet()) {
+            encodedMap.put(c, "");
+        }
         HuffmanNode current = t.root;
         HuffmanNode parent = t.root;
         String s = "";
-        int counter = 0;
-        while (current != null){
-            if (current.letter == '~' && current.leftChild != null){
+        while (current != null) {
+            if (current.letter == '~' && current.leftChild != null) {
                 parent = current;
                 current = current.leftChild;
                 s += "0";
-            } else if (current.letter != '~'){
-                counter ++;
-                m.put(current.letter,Integer.parseInt(s));
-                if(s.endsWith("0")){
+            } else if (current.letter != '~' || (current.leftChild == null && current.rightChild == null)) {
+                if (current.letter != '~') {
+                    encodedMap.put(current.letter, s);
+                }
+
+                if (s.endsWith("0")) {
                     parent.leftChild = null;
-                } else {
+                } else if (s.endsWith("1")) {
                     parent.rightChild = null;
+                } else if (s.equals("")) {
+                    break;
                 }
                 current = t.root;
                 parent = t.root;
                 s = "";
-                if (counter == m.size()){
-                    break;
-                }
             } else {
                 parent = current;
                 current = current.rightChild;
                 s += "1";
             }
         }
-        System.out.println("\nEncoded Map:  " + m.toString());
-        encodedMap = m;
+        System.out.println("\nEncoded Map:  " + encodedMap.toString());
     }
 
-    public static void encodeString(){
+    public static String encodeString() {
         char[] ca = message.toCharArray();
         String s = "";
-        for (char c : ca){
+        for (char c : ca) {
             s = s + encodedMap.get(c) + " ";
         }
         System.out.println("\nEncoded message: " + s);
+        return s;
+    }
+
+    public static void decodeString(String encodedString) {
+        String[] sa = encodedString.split(" ");
+        String decodedString = "";
+        HuffmanNode current;
+        for (String s : sa) {
+            char[] ca = s.toCharArray();
+            current = huffmanTree.root;
+            for (char c : ca) {
+                if (c == '0') {
+                    current = current.leftChild;
+                } else if (c == '1') {
+                    current = current.rightChild;
+                }
+            }
+            decodedString += current.letter;
+        }
+        System.out.println("\nDecoded message: " + decodedString);
     }
 
     public static void main(String[] args) {
         makeSortedMap(input());
         makeHuffmanTree();
         encodeTree();
-        encodeString();
+        decodeString(encodeString());
     }
 }
